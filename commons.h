a2be0ca8a5687200d4ab2b4c9b1275ea32dc557d
Mikołaj Florkiewicz <florek@florkiewicz.cu.cc>
2015-04-30 13:17:47 +0200

    lol no tab

--- commons.h
+++ commons.h
@@ -12,6 +12,6 @@
 
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args) {
-	        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
